d8ce781f9c93e7960f1d8a2b6549bb808b72c52d
Carlos Antonio Minero Dubon <carlosminerodubon@gmail.com>
2020-04-30 15:28:04 -0600

    create option image test 4

--- src/routes/opcion.js
+++ src/routes/opcion.js
@@ -66,8 +66,8 @@ router.get("/ws/opciones/:id", async (req, res) => {
 router.post("/ws/opciones", [auth], async (req, res) => {
 
    /* try {*/
-    const { nombre, precio, step, descripcion } = req.body;
-    const icon = req.files.icono;
+        const { nombre, precio, step, descripcion } = req.body;
+        const icon = req.files.icono;
 
         /*let nombreCortado = icon.name.split(".");
         let extension = nombreCortado[nombreCortado.length - 1];
@@ -79,22 +79,25 @@ router.post("/ws/opciones", [auth], async (req, res) => {
             }
         });*/
 
-    cloudinary.uploader.upload_stream((err,result) =>{
-        const nuevaOpcion = new Opcion({
-            nombre,
-            step,
-            descripcion,
-            icono: result.secure_url,
-            precio
-        });
-        nuevaOpcion.save((err,nOpcion)=>{
-            if(err) return res.status(500).json({mensaje:"Error interno en servidor al guardar opcion: " + err,status:false})
-            res.json({
-                opcion: nOpcion,
-                status: true
+        cloudinary.uploader.upload(icon.name,(error,result)=>{
+            if(error) return res.status(500).json({mensaje:"Error interno en servidor al subir imagen: " + err,status:false})
+
+            const nuevaOpcion = new Opcion({
+                nombre,
+                step,
+                descripcion,
+                icono: result.secure_url,
+                precio
             });
-        });
-    }).end(icon)
+            nuevaOpcion.save((err,nOpcion)=>{
+                if(err) return res.status(500).json({mensaje:"Error interno en servidor al guardar opcion: " + err,status:false})
+                res.json({
+                    opcion: nOpcion,
+                    status: true
+                });
+            });
+
+        })
 
 
     /*} catch (err) {
